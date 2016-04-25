74239f1891c713746d34ed753207291aa0d7802e
tanyaweaver <tanya83@uw.edu>
2016-04-25 14:53:25 -0700

    3 random pictures to the screen

--- js/app.js
+++ js/app.js
@@ -5,8 +5,9 @@ function MallItem (name, path) {
   this.itemPath = path;
   this.timesShown = 0;
   this.timesVoted = 0;
+  this.popularity = 0;
   allItemsArray.push(this);
-}
+};
 
 MallItem.prototype.itemShown = function() {
   //if(){this.timesShown++}
@@ -19,7 +20,26 @@ MallItem.prototype.itemVoted = function() {
 };
 
 MallItem.prototype.itemPopularity = function() {
-  return (this.itemVoted() / this.itemShown());
+  this.popularity = this.itemVoted() / this.itemShown();
+  return this.popularity;
 };
 
-var bag = new MallItem ('bag', '../images/bag.jpg');
+var bag = new MallItem ('bag', 'images/bag.jpg');
+var banana = new MallItem ('banana', 'images/banana.jpg');
+var bathroom = new MallItem ('bathroom', 'images/bathroom.jpg');
+var boots = new MallItem ('boots', 'images/boots.jpg');
+var breakfast = new MallItem ('breakfast', 'images/breakfast.jpg');
+var bubblegum = new MallItem ('bubblegum', 'images/bubblegum.jpg');
+
+function getRandomItem(){
+  return Math.floor(Math.random() * allItemsArray.length);
+};
+
+var threeImageContainer = document.getElementById('three-image-container');
+function displayThreeImages (){
+  for (var i = 0; i < 3; i++){
+    var singleImage = document.createElement('div');
+    singleImage.innerHTML = '<img src="' + allItemsArray[getRandomItem()].itemPath + '">';
+    threeImageContainer.appendChild(singleImage);
+  }
+}
